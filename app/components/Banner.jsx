0d0c5210b3534fded0dbb4dcac7ce8f2842af93c
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2023-09-28 12:44:38 +0500

    build issue resolved
    Window depandancy of water ripple removed

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -1,54 +1,12 @@
 // import "@/public/jquery.ripples"
 'use client'
-import { useEffect } from "react"
 import bg from "@/app/assets/rocket.jpg"
 import Tilt from 'react-parallax-tilt'
-import { WaterCanvas, WaterModel } from "@/public/watercanvas"
+// import { WaterCanvas, WaterModel } from "@/public/watercanvas"
 import Image from "next/image"
-import Script from "next/script"
+// import Script from "next/script"
 
 const Banner = ({ }) => {
-  useEffect(() => {
-    let doc = document.getElementById("waterHolder")
-    console.log(doc, "waterHolder")
-  }, [])
-  useEffect(() => {
-    // JavaScript code for initializing the Water Ripple effect
-    const width = 350;
-    const height = 275;
-
-    function init() {
-      var waterModel = new WaterModel(width, height, {
-        // Your WaterModel configuration here
-        resolution: 2.0,
-        interpolate: false,
-        damping: 0.985,
-        clipping: 5,
-        evolveThreshold: 0.05,
-        maxFps: 30,
-        showStats: true
-      });
-
-      var waterCanvas = new WaterCanvas(width, height, "waterHolder", waterModel, {
-        // Your WaterCanvas configuration here
-        backgroundImageUrl: bg,
-        lightRefraction: 9.0,
-        lightReflection: 0.1,
-        maxFps: 20,
-        showStats: true
-      });
-
-      var finger = [
-        [0.5, 1.0, 0.5],
-        [1.0, 1.0, 1.0],
-        [0.5, 1.0, 0.5]
-      ];
-
-      waterModel.touchWater(100, 100, 1.5, finger);
-    }
-    // Call the init function when the component mounts
-    // init();
-  }, []);
   return (
     <>
       <Tilt>
@@ -61,9 +19,9 @@ const Banner = ({ }) => {
           {/* <p className='absolute bottom-20'>Scroll down</p> */}
         </section>
       </Tilt>
-      <Script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js" />
-      <Script src="../../public/ripple.js"/>
-      <Script src="../../public/watercanvas.js" />
+      {/* <Script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js" />
+      <Script src="../../public/ripple.js" />
+      <Script src="../../public/watercanvas.js" /> */}
     </>
   )
 }
